bc1bab5070f694a022fd1819867522f12a3e555d
Alex Ventura <alex.rv11@gmail.com>
2024-10-22 04:36:40 -0300

    feat: denife and use the header component

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface HeaderProps {
+  
+}
+ 
+const Header: React.FC<HeaderProps> = () => {
+  return (<div>
+    Header content
+  </div>);
+}
+ 
+export default Header;
